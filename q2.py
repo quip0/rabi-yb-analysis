4ddd08dfb6edf7a539966de0354a17eff28a2024
aelskf <105953651+aelskf@users.noreply.github.com>
2026-08-09 20:10:23 -1000

    upload q2
    use the power of error propogation
    
    however, the data is pretty small so im not sure how accurate this is

---
+++ q2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+
+#Load txt file
+data = np.loadtxt('data.txt', delimiter='\t')
+print(data.shape)
+
+#Take average of columns
+averages=np.mean(data, axis=0)
+print(averages)
+print(averages.shape)
+
+#plot stuff
+steps=np.arange(0, 200, 2)
+print(steps)
+plt.plot(steps, averages, marker='o', label='test')
+
+
+def functionone(t, offset, amp, freq, phase):
+    return offset+amp*np.sin(2*np.pi*freq*t + phase)
+
+p0=[averages.mean(), (averages.max()-averages.min())/2, 1/(20), -np.pi/2]
+popt, pcov = scipy.optimize.curve_fit(functionone, steps, averages, p0)
+print(popt)
+fit_freq=popt[2]
+pulse = 0.5/fit_freq
+print(f"The pi pulse length is {pulse} microseconds.")
+
+#calculate errors
+perr = np.sqrt(np.diag(pcov))
+freq_error = perr[2]
+pulse_error = (0.5/(fit_freq**2) )*freq_error
+
+print(f"The error in pi pulse length is {pulse_error} microseconds.")
+plt.legend()
+plt.show() 
+
